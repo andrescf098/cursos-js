e0c378c74417bc17069c52be539acf0b6797336a
AndresCifuentes <andrescf@outlook.com>
2022-12-12 16:27:33 -0500

    Reto completado

--- curso-react/src/routes/App.jsx
+++ curso-react/src/routes/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
-import Login from "../containers/Login";
-import RecoveryPassword from "../containers/RecoveryPassword";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
+import RecoveryPassword from "../pages/RecoveryPassword";
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import "../styles/global.css";
 
 const App = () => {
@@ -15,6 +21,12 @@ const App = () => {
           <Route path="/" exact element={<Home/>} />
           <Route path="/login" exact element={<Login/>} />
           <Route path="/recovery-password" exact element={<RecoveryPassword/>} />
+          <Route path="/send-email" exact element={<SendEmail/>}/>
+          <Route path="/new-password" exact element={<NewPassword/>}/>
+          <Route path="/account" exact element={<MyAccount/>} />
+          <Route path="/signup" exact element={<CreateAccount/>} />
+          <Route path="/checkout" exact element={<Checkout/>} />
+          <Route path="/orders" exact element={<Orders/>} />
           <Route path="*" element={<NotFound/>} />
         </Routes>
       </Layout>
